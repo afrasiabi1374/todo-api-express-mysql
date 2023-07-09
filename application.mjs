45a54dd6541212b8010d04401e5d6a408de380f0
afrasiabi1374 <mohammad.aligiga@gmail.com>
2023-07-09 18:10:19 +0430

    ali

--- application.mjs
+++ application.mjs
@@ -10,18 +10,7 @@ class Application
     {
         this.#initExpress()
         this.#initRoute()
-        const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'dbtodo'
-        })
-        connection.connect()
-        connection.query('SELECT * From todos', (err, rows, fields) => {
-        if (err) throw err
-        console.log('The solution is: ', rows)
-        })
-        connection.end()
+
     }
 
     async #initExpress(){
--- model/db.mjs
+++ model/db.mjs
@@ -1,10 +1,21 @@
-class InitDb
+class Db
 {
     constructor(){
 
-
-
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'dbtodo'
+            })
+            connection.connect()
+            connection.query('SELECT * From todos', (err, rows, fields) => {
+            if (err) throw err
+            console.log('The solution is: ', rows)
+            })
+            connection.end()
 
     }
-    dest
+    
 }
+export default new Db()
