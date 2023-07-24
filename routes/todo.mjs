2edaa9c4075b25f87eeaab343c45d0c6803428be
afrasiabi1374 <mohammad.aligiga@gmail.com>
2023-07-24 17:35:07 +0430

    a

--- routes/todo.mjs
+++ routes/todo.mjs
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import TodoController from "../controllers/TodoController.mjs";
 import { log } from "../core/utils.mjs";
+import  cors from "cors";
 const route = Router()
-route.get('/', TodoController.all)
-route.post('/add', TodoController.add)
-route.delete('/delete', TodoController.deleteTodo)
-route.put('/update', TodoController.updateTodo)
-route.put('/did', TodoController.updateTodoDid)
+route.get('/', cors() ,TodoController.all)
+route.post('/add', cors(), TodoController.add)
+route.delete('/delete', cors(), TodoController.deleteTodo)
+route.put('/update', cors(), TodoController.updateTodo)
+route.put('/did', cors(), TodoController.updateTodoDid)
 
 export default route
