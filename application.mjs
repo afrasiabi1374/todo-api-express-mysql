338fb7401b1fc46b3675d4881542c8b81d732a65
afrasiabi1374 <mohammad.aligiga@gmail.com>
2023-07-29 08:14:40 +0430

    1

--- application.mjs
+++ application.mjs
@@ -1,6 +1,8 @@
 import { getEnv, isJSON, log, random, sleep } from "./core/utils.mjs";
 import Express from 'express'
 import route from './routes/route.mjs'
+import  cors from "cors";
+
 class Application
 {
 
@@ -21,6 +23,7 @@ class Application
         this.#app.use(Express.json({
             limit: '10mb'
         }))
+        this.#app.use(cors())
 
     }
 
